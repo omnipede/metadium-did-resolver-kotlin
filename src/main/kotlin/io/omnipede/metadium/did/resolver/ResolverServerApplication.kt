6bd750f1539f3752a8fce300f3bd808498e5f36d
서현규 <omnipede@coinplug.com>
2021-06-04 16:30:38 +0900

    기타: ResolverApplication 클래스명을 ResolverServerApplication 으로 수정함

--- src/main/kotlin/io/omnipede/metadium/did/resolver/ResolverServerApplication.kt
+++ src/main/kotlin/io/omnipede/metadium/did/resolver/ResolverServerApplication.kt
@@ -2,12 +2,10 @@ package io.omnipede.metadium.did.resolver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class ResolverApplication
+class ResolverServerApplication
 
 fun main(args: Array<String>) {
-    runApplication<ResolverApplication>(*args)
+    runApplication<ResolverServerApplication>(*args)
 }
