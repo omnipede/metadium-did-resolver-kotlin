30d0ca6efa4849f5e23d0441c29ca0d1e7fd8c25
서현규 <omnipede@coinplug.com>
2021-06-04 14:41:49 +0900

    기타: 불필요한 코드 삭제

--- src/main/kotlin/io/omnipede/metadium/did/resolver/ResolverServerApplication.kt
+++ src/main/kotlin/io/omnipede/metadium/did/resolver/ResolverServerApplication.kt
@@ -6,23 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-open class ResolverApplication
+class ResolverApplication
 
 fun main(args: Array<String>) {
     runApplication<ResolverApplication>(*args)
 }
-
-@RestController
-class TempController {
-
-    @GetMapping("/api/v1/hello")
-    fun index(): List<Message> {
-
-        return listOf(
-            Message("1", "Hello"),
-            Message("2", "Kotlin")
-        )
-    }
-}
-
-data class Message(val id: String, val contents: String)
