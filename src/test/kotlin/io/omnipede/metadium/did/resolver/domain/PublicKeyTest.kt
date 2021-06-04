053847093f1af493eb59659cb187554b8fe73e9e
서현규 <omnipede@coinplug.com>
2021-06-04 14:08:32 +0900

    테스트: PublicKeyTest 수정
    테스트코드 가독성을 높이기 위해 불필요한 parameterized test 를 일반 test 로 수정함.

--- src/test/kotlin/io/omnipede/metadium/did/resolver/domain/PublicKeyTest.kt
+++ src/test/kotlin/io/omnipede/metadium/did/resolver/domain/PublicKeyTest.kt
@@ -2,6 +2,8 @@ package io.omnipede.metadium.did.resolver.domain
 
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.assertj.core.api.AssertionsForInterfaceTypes.catchThrowable
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -10,34 +12,17 @@ import java.util.stream.Stream
 
 internal class PublicKeyTest {
 
-    companion object {
-        /**
-         * PublicKey 객체 생성 테스트 시 사용할 argument set 을 반환하는 메소드
-         * @return Argument (did, address, publicKeyHex) 리스트
-         */
-        @JvmStatic
-        fun argumentProvider(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.of(
-                    "did:meta:000000000000000000000000000000000000000000000000000000000000112b",
-                    "0x0c65a336fc97d4cf830baeb739153f312cbefcc9", null),
-                Arguments.of(
-                    "did:meta:000000000000000000000000000000000000000000000000000000000000112b",
-                    "0x0c65a336fc97d4cf830baeb739153f312cbefcc9", "0x0c65a336fc97d4cf830baeb739153f312cbefcc9")
-            )
-        }
-    }
-
-    @ParameterizedTest(name = "PublicKey 객체 생성 테스트: {index}")
-    @MethodSource("argumentProvider")
-    fun publicKey_객체_생성(did: String, address: String, publicKeyHex: String?) {
+    @Test
+    @DisplayName("DID, address 이용해서 PublicKey 객체 생성")
+    fun publicKey_constructor_test_1() {
         // Given
+        val did = "did:meta:000000000000000000000000000000000000000000000000000000000000112b"
         val keyId = "Testing"
+        val address = "0x0c65a336fc97d4cf830baeb739153f312cbefcc9"
+        val publicKeyHex = "0x0c65a336fc97d4cf830baeb739153f312cbefcc9"
 
         // When
-        val publicKey = if (publicKeyHex == null)
-            PublicKey(did=did, keyId=keyId, address=address)
-        else PublicKey(did=did, keyId=keyId, address=address, publicKey = publicKeyHex)
+        val publicKey = PublicKey(did=did, keyId=keyId, address=address, publicKey=publicKeyHex)
 
         // Then
         assertThat(publicKey).isNotNull
@@ -45,17 +30,34 @@ internal class PublicKeyTest {
         assertThat(publicKey.controller).isEqualTo(did)
         assertThat(publicKey.publicKeyHash).isEqualTo(address.substring(2).toLowerCase())
         assertThat(publicKey.id).isEqualTo("$did#$keyId#${address.substring(2).toLowerCase()}")
-
-        if (publicKeyHex == null) assertThat(publicKey.publicKeyHex).isNull()
-        else assertThat(publicKey.publicKeyHex).isEqualTo(publicKeyHex.substring(2).toLowerCase())
+        assertThat(publicKey.publicKeyHex).isEqualTo(publicKeyHex)
     }
 
-    @ParameterizedTest(name = "DID 형식 validation 테스트: {0}")
-    @ValueSource(strings = [
-        "did:Zeta:000000000000000000000000000000000000000000000000000000000000112b", // 'Z'
-    ])
-    fun did_형식이_이상할_때_IllegalArgumentException_발생하는지_확인(did: String) {
+    @Test
+    @DisplayName("DID, address, publicKeyHex 이용해서 PublicKey 객체 생성")
+    fun publicKey_constructor_test_2() {
         // Given
+        val did = "did:meta:000000000000000000000000000000000000000000000000000000000000112b"
+        val keyId = "Testing"
+        val address = "0x0c65a336fc97d4cf830baeb739153f312cbefcc9"
+
+        // When
+        val publicKey = PublicKey(did=did, keyId=keyId, address=address)
+
+        // Then
+        assertThat(publicKey).isNotNull
+        assertThat(publicKey.type).isEqualTo("EcdsaSecp256k1VerificationKey2019")
+        assertThat(publicKey.controller).isEqualTo(did)
+        assertThat(publicKey.publicKeyHash).isEqualTo(address.substring(2).toLowerCase())
+        assertThat(publicKey.id).isEqualTo("$did#$keyId#${address.substring(2).toLowerCase()}")
+        assertThat(publicKey.publicKeyHex).isNull()
+    }
+
+    @Test
+    @DisplayName("DID 형식 validation 테스트: {0}")
+    fun did_형식이_이상할_때_IllegalArgumentException_발생하는지_확인() {
+        // Given
+        val did = "did:Zeta:000000000000000000000000000000000000000000000000000000000000112b"
         val keyId = "Testing"
         val address = "0c65a336fc97d4cf830baeb739153f312cbefcc9"
         val publicKeyHex = "0c65a336fc97d4cf830baeb739153f312cbefcc9"
@@ -71,14 +73,13 @@ internal class PublicKeyTest {
         assertThat(throwable).hasMessageContaining("Invalid DID format: $did")
     }
 
-    @ParameterizedTest(name = "Address 형식 validation 테스트: {0}")
-    @ValueSource(strings = [
-        "c", // Too short
-    ])
-    fun address_형식이_이상할_때_IllegalArgumentException_발생하는지_확인(address: String) {
+    @Test
+    @DisplayName("Address 형식 validation 테스트")
+    fun address_형식이_이상할_때_IllegalArgumentException_발생하는지_확인() {
         // Given
         val did = "did:meta:000000000000000000000000000000000000000000000000000000000000112b"
         val keyId = "Testing"
+        val address = "c"
         val publicKeyHex = "0c65a336fc97d4cf830baeb739153f312cbefcc9"
 
         // When
@@ -92,15 +93,14 @@ internal class PublicKeyTest {
         assertThat(throwable).hasMessageContaining("Invalid address format: $address")
     }
 
-    @ParameterizedTest(name = "PublicKey 형식 validation 테스트: {0}")
-    @ValueSource(strings = [
-        "c", // Too short
-    ])
-    fun publicKeyHex_형식이_이상할_때_IllegalArgumentException_발생하는지_확인(publicKey: String) {
+    @Test
+    @DisplayName("PublicKey 형식 validation 테스트")
+    fun publicKeyHex_형식이_이상할_때_IllegalArgumentException_발생하는지_확인() {
         // Given
         val did = "did:meta:000000000000000000000000000000000000000000000000000000000000112b"
         val keyId = "Testing"
         val address = "0c65a336fc97d4cf830baeb739153f312cbefcc9"
+        val publicKey = "c"
 
         // When
         val throwable: Throwable? = catchThrowable {
