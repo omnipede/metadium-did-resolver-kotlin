0fc68502347cc9e84f51da766bbecf1d7ef4abbc
서현규 <omnipede@coinplug.com>
2021-06-15 10:55:40 +0900

    리팩토링: domain.entity.DidDocument 수정
    불필요한 mutable property 를 immutable 하게 수정함

--- src/main/kotlin/io/omnipede/metadium/did/resolver/domain/entity/DidDocument.kt
+++ src/main/kotlin/io/omnipede/metadium/did/resolver/domain/entity/DidDocument.kt
@@ -9,24 +9,30 @@ import java.util.stream.Collectors
 class DidDocument(did: String) {
     val context = "https://w3id.org/did/v0.11"
     val id = did
-    val publicKeyList: MutableList<PublicKey> = mutableListOf()
-    val authenticationList: MutableList<String> = mutableListOf()
-    val associatedServiceList: MutableList<AssociatedService> = mutableListOf()
+    var publicKeyList: List<PublicKey>
+        private set
+    var authenticationList: List<String>
+        private set
+    var associatedServiceList: List<AssociatedService>
+        private set
 
     init {
         // String validation
         if (!id.isValidDid())
             throw IllegalArgumentException("Invalid DID format: $did")
+        publicKeyList = emptyList()
+        authenticationList = emptyList()
+        associatedServiceList = emptyList()
     }
 
     constructor(did: String, publicKeyList: List<PublicKey>): this(did) {
-        this.publicKeyList += publicKeyList
+        this.publicKeyList = publicKeyList
         // PublicKey 의 ID 만 따로 모아서 authentication list 를 새로 만든다
-        this.authenticationList += this.publicKeyList
+        this.authenticationList = this.publicKeyList
             .stream().parallel().map { it.id }.collect(Collectors.toList())
     }
 
     constructor(did: String, publicKeyList: List<PublicKey>, associatedServiceList: List<AssociatedService>): this(did, publicKeyList) {
-        this.associatedServiceList += associatedServiceList
+        this.associatedServiceList = associatedServiceList
     }
 }
