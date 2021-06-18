a260c00aaa2ddcfa06c4ac781ab51c0115020ab6
omnipede <omnipede@naver.com>
2021-06-18 11:18:40 +0900

    기능: domain.application.ResolverApplication 수정
    Document 조회 시, 캐시된 document 가 존재할 경우 캐시된 document 를 반환하도록 코드를 수정함

--- src/main/kotlin/io/omnipede/metadium/did/resolver/domain/application/ResolverApplication.kt
+++ src/main/kotlin/io/omnipede/metadium/did/resolver/domain/application/ResolverApplication.kt
@@ -3,13 +3,10 @@ package io.omnipede.metadium.did.resolver.domain.application
 import arrow.core.Either
 import arrow.core.getOrHandle
 import arrow.core.left
-import io.omnipede.metadium.did.resolver.domain.ports.ContractService
-import io.omnipede.metadium.did.resolver.domain.ports.EnvService
-import io.omnipede.metadium.did.resolver.domain.ports.MetaData
 import io.omnipede.metadium.did.resolver.domain.entity.AssociatedService
 import io.omnipede.metadium.did.resolver.domain.entity.DidDocument
 import io.omnipede.metadium.did.resolver.domain.entity.MetadiumDID
-import io.omnipede.metadium.did.resolver.domain.ports.NotFoundIdentityException
+import io.omnipede.metadium.did.resolver.domain.ports.*
 import org.springframework.stereotype.Service
 
 /**
@@ -18,7 +15,8 @@ import org.springframework.stereotype.Service
 @Service
 class ResolverApplication(
     private val contractService: ContractService,
-    private val envService: EnvService
+    private val envService: EnvService,
+    private val documentCache: DocumentCache
 ) {
     /**
      * DID 의 DID document 를 컨트랙으로부터 읽어서 반환하는 메소드
@@ -38,15 +36,39 @@ class ResolverApplication(
         if (currentNetwork != metadiumDID.net)
             return Either.Left(ResolverError.DifferentNetwork("This server is DID resolver for $currentNetwork"))
 
-        // Resolve did document
-        val document = resolveDocumentFromContract(metadiumDID)
-            .getOrHandle { return Either.Left(ResolverError.NotFoundIdentity(it.message!!)) }
+        // 먼저 캐시에서 document 를 찾는다
+        val cachedDocument = documentCache.find(metadiumDID)
+
+        // 찾는데 성공했다면 해당 document 를 반환
+        val document = if (cachedDocument.isPresent) {
+            // 메타데이타에 캐시되었음을 표기
+            metaData.markCached()
+            cachedDocument.get()
+        } else {
+            // 캐시 되지 않았다면 contract 에서 찾는다
+            resolveDocumentFromContract(metadiumDID)
+                .getOrHandle { return Either.Left(ResolverError.NotFoundIdentity(it.message!!)) }
+        }
 
         // Mark metadata that resolving process is end
         metaData.endResolving()
 
         // Return document and metadata
         return Either.Right(document to metaData)
+    }
+
+    /**
+     * 캐시된 document 를 삭제하는 메소드. 캐시된 document 가 없으면 false 를 반환하고 삭제에 성공하면 true 를 반환한다
+     * @param did DID for document
+     * @return 캐시된 document 가 없으면 false 를 반환하고 삭제에 성공하면 true 를 반환한다
+     */
+    fun deleteDocumentFromCache(did: String): Boolean {
+
+        // FROM DID, get metaId
+        val metadiumDID = MetadiumDID(did)
+
+        // 캐시에서 삭제한다
+        return documentCache.delete(metadiumDID)
     }
 
     /**
@@ -72,6 +94,9 @@ class ResolverApplication(
             publicKeyList = publicKeyListResult.toList(),
             serviceList
         )
+
+        // Save document to cache
+        documentCache.save(document)
 
         return Either.Right(document)
     }
